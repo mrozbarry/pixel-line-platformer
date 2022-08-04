8eb6219888e47f44d0b41a3fde0fa26635f49573
Alex Barry <alex.barry@gmail.com>
2022-08-04 11:23:59 -0400

    Copied default level

--- src/draw.js
+++ src/draw.js
@@ -1,15 +1,29 @@
 import { render, c } from 'declarativas';
-import * as CollisionMap from './collisionMap.js';
 
 const SCALE = 1.5;
 
 export const draw = (state) => {
-  const { entities, geometry } = CollisionMap.collidables(state.entities, state.level);
+  const levelSize = {
+    x: state.level.length * 16,
+    y: state.level[0].length * 16,
+  };
+  const canvasSize = {
+    x: state.canvas.width,
+    y: state.canvas.height,
+  };
+
+  let scale = SCALE;
+  if (canvasSize.x > canvasSize.y) {
+    scale = canvasSize.x / levelSize.x / 2;
+  } else {
+    scale = canvasSize.y / levelSize.y / 2;
+  }
+
   render(
     state.canvas.getContext('2d'),
     [
       c('save'),
-      c('scale', { x: SCALE, y: SCALE }),
+      c('scale', { x: scale, y: scale }),
       state.level.map((row, y) => row.map((cell, x) => state.assets.render({ index: cell, x: 16 * x, y: 16 * y }))),
       state.entities.map(e => [
         state.assets.render({
@@ -21,24 +35,6 @@ export const draw = (state) => {
           mirror: e.mirror,
         }),
       ]),
-      // ...geometry.map(g => [
-      //   c('strokeStyle', { value: 'red' }),
-      //   c('strokeRect', {
-      //     x: g.topLeft.x,
-      //     y: g.topLeft.y,
-      //     width: g.bottomRight.x - g.topLeft.x,
-      //     height: g.bottomRight.y - g.topLeft.y,
-      //   }),
-      // ]),
-      // ...[...entities.values()].map(e => [
-      //   c('strokeStyle', { value: 'green' }),
-      //   c('strokeRect', {
-      //     x: e.topLeft.x,
-      //     y: e.topLeft.y,
-      //     width: e.bottomRight.x - e.topLeft.x,
-      //     height: e.bottomRight.y - e.topLeft.y,
-      //   }),
-      // ]),
       c('restore'),
     ],
   );
